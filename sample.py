895fa8919f67ddf111db416f5173ba27aa9d34fa
c-basalt <117849907+c-basalt@users.noreply.github.com>
2023-09-03 12:54:32 -0400

    simplify client

--- sample.py
+++ sample.py
@@ -2,7 +2,7 @@
 import asyncio
 import random
 
-import blivedm
+from blivedm.client import BLiveClient
 
 # 直播间ID的取值看直播间URL
 TEST_ROOM_IDS = [
@@ -12,6 +12,20 @@ TEST_ROOM_IDS = [
     21449083,
     23105590,
 ]
+
+
+class BaseHandler():
+    _CMD_CALLBACK_DICT = {}
+
+    async def handle(self, client, command: dict):
+        cmd = command.get('cmd', '').split(':')[0]
+
+        if cmd not in self._CMD_CALLBACK_DICT:
+            print(cmd, command)
+        else:
+            callback = self._CMD_CALLBACK_DICT[cmd]
+            if callback is not None:
+                await callback(self, client, command)
 
 
 async def main():
@@ -25,8 +39,8 @@ async def run_single_client():
     """
     room_id = random.choice(TEST_ROOM_IDS)
     # 如果SSL验证失败就把ssl设为False，B站真的有过忘续证书的情况
-    client = blivedm.BLiveClient(room_id, ssl=True)
-    handler = MyHandler()
+    client = BLiveClient(room_id, ssl=True)
+    handler = BaseHandler()
     client.add_handler(handler)
 
     client.start()
@@ -44,8 +58,8 @@ async def run_multi_clients():
     """
     演示同时监听多个直播间
     """
-    clients = [blivedm.BLiveClient(room_id) for room_id in TEST_ROOM_IDS]
-    handler = MyHandler()
+    clients = [BLiveClient(room_id) for room_id in TEST_ROOM_IDS]
+    handler = BaseHandler()
     for client in clients:
         client.add_handler(handler)
         client.start()
@@ -58,33 +72,6 @@ async def run_multi_clients():
         await asyncio.gather(*(
             client.stop_and_close() for client in clients
         ))
-
-
-class MyHandler(blivedm.BaseHandler):
-    # # 演示如何添加自定义回调
-    # _CMD_CALLBACK_DICT = blivedm.BaseHandler._CMD_CALLBACK_DICT.copy()
-    #
-    # # 入场消息回调
-    # async def __interact_word_callback(self, client: blivedm.BLiveClient, command: dict):
-    #     print(f"[{client.room_id}] INTERACT_WORD: self_type={type(self).__name__}, room_id={client.room_id},"
-    #           f" uname={command['data']['uname']}")
-    # _CMD_CALLBACK_DICT['INTERACT_WORD'] = __interact_word_callback  # noqa
-
-    async def _on_heartbeat(self, client: blivedm.BLiveClient, message: blivedm.HeartbeatMessage):
-        print(f'[{client.room_id}] 当前人气值：{message.popularity}')
-
-    async def _on_danmaku(self, client: blivedm.BLiveClient, message: blivedm.DanmakuMessage):
-        print(f'[{client.room_id}] {message.uname}：{message.msg}')
-
-    async def _on_gift(self, client: blivedm.BLiveClient, message: blivedm.GiftMessage):
-        print(f'[{client.room_id}] {message.uname} 赠送{message.gift_name}x{message.num}'
-              f' （{message.coin_type}瓜子x{message.total_coin}）')
-
-    async def _on_buy_guard(self, client: blivedm.BLiveClient, message: blivedm.GuardBuyMessage):
-        print(f'[{client.room_id}] {message.username} 购买{message.gift_name}')
-
-    async def _on_super_chat(self, client: blivedm.BLiveClient, message: blivedm.SuperChatMessage):
-        print(f'[{client.room_id}] 醒目留言 ¥{message.price} {message.uname}：{message.message}')
 
 
 if __name__ == '__main__':
